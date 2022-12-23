d95738586e930496051a4d398d9c604ef9c0b7e6
unknown <69178977+KaliFizz@users.noreply.github.com>
2022-12-23 18:54:20 -0300

    add light/dark themes & footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,16 +5,71 @@ function scrollHeader() {
   else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
-/*=============== SERVICES MODAL ===============*/
-
-/*=============== MIXITUP FILTER PORTFOLIO ===============*/
-
-/* Link active work */
-
-/*=============== SWIPER TESTIMONIAL ===============*/
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.pageYOffset
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active-link')
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link')
+    }
+  })
+}
+
+window.addEventListener(`scroll`, scrollActive)
 
 /*=============== LIGHT DARK THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const lightTheme = 'light-theme'
+const iconTheme = 'bx-sun'
 
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-theme')
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    lightTheme,
+  )
+  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](
+    iconTheme,
+  )
+}
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(lightTheme)
+  themeButton.classList.toggle(iconTheme)
+
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+})
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2000,
+  delay: 400,
+})
+
+sr.reveal(`.home__data`)
+sr.reveal(`.home__handle`, { delay: 260 })
+sr.reveal(`.home__data, .home__scroll, .home__social`, {
+  delay: 900,
+  origin: 'bottom',
+})
