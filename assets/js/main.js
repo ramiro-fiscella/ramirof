fc4597c9280e60e5af5d3fe7c5e8339f5e28bcb8
unknown <69178977+KaliFizz@users.noreply.github.com>
2023-01-17 13:31:56 -0300

    menu for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
+/*=============== MENU HAMBURGUER ===============*/
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
+}
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
+
+/*=============== REMOVE MENU ON MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction))
+
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
   const header = document.querySelector('header')
